6434b00663080ce5057e65d02902e7e44026271f
Sergey Plotnikov <splotnikov@gmail.com>
2026-03-26 16:13:30 -0400

    feat: initial commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,6 @@
-// src/lib/utils.ts
-
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
-
-// Format price from cents to currency string
-// 3000 → "$30.00"
-export function formatPrice(cents: number, currency = 'USD') {
-	return new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency,
-	}).format(cents / 100);
+  return twMerge(clsx(inputs))
 }
